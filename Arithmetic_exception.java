f2e22162e9d06aac2b3c341f4966ef86cea4c512
sushant2106 <90310743+sushant2106@users.noreply.github.com>
2023-04-05 16:05:00 +0530

    Create Arithmetic_exception.java

---
+++ Arithmetic_exception.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	  int arr[]
+	  try{
+	        k=i/j;
+	  }
+	  catch(ArithmeticException e)
+	  {
+	      System.out.println("can not divide by zero");
+	  }
+	}
+}
